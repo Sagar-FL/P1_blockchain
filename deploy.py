e7cc86d31f2c67b0b1aa3ee5f48adf02bde8de35
Sagar Freelancer <pogirikalasagar2@gmail.com>
2022-02-22 10:10:44 +0530

    Added web3 testnet contract deployment support with ganache

--- deploy.py
+++ deploy.py
@@ -1,5 +1,10 @@
 import json
 from solcx import compile_standard, install_solc
+from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -35,4 +40,56 @@ abi = json.loads(
     compile_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
 )["output"]["abi"]
 
-print(abi)
+# print(abi)
+
+# connect to ganache chain
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:8545"))
+chain_id = 1337
+my_address = os.getenv("MY_GANACHE_ADDRESS")
+private_key = os.getenv("PRIVATE_KEY")
+
+# create a contract
+print("Deploying contract")
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+print("Deploying complete")
+
+# Get latest nounce
+nonce = w3.eth.getTransactionCount(my_address)
+
+print("Creating txn")
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+print("Signing txn")
+signed_transaction = w3.eth.account.sign_transaction(
+    transaction, private_key=private_key
+)
+tx_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+nonce += 1
+print("Txn complete")
+
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+print("Creating txn")
+store_tx = simple_storage.functions.store(143).buildTransaction(
+    {
+        "gasPrice": w3.eth.gas_price,
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+print("Signing txn")
+signed_store_tx = w3.eth.account.sign_transaction(store_tx, private_key=private_key)
+store_tx_hash = w3.eth.send_raw_transaction(signed_store_tx.rawTransaction)
+store_tx_recipt = w3.eth.wait_for_transaction_receipt(store_tx_hash)
+nonce += 1
+print("Txn complete")
+
+print(simple_storage.functions.retrive().call())
